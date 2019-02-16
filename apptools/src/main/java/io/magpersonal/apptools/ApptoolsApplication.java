da8edf645ed6eaf0fcb1e0f25c976a97640aa6fe
kolalimorteza <kolalimorteza@yahoo.com>
2019-02-16 13:03:12 +0330

    App React Java API

---
+++ apptools/src/main/java/io/magpersonal/apptools/ApptoolsApplication.java
@@ -0,0 +1,14 @@
+package io.magpersonal.apptools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApptoolsApplication.class, args);
+    }
+
+}
+
